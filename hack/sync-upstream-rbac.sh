56037171386ede75dd7636aa8b5621d963418e37
Andrey Lebedev <alebedev@redhat.com>
2022-04-11 21:43:47 +0200

    rbac: operand role moved to the bundle

--- hack/sync-upstream-rbac.sh
+++ hack/sync-upstream-rbac.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 UPSTREAM="https://raw.githubusercontent.com/openshift/aws-load-balancer-controller/main/config/rbac"
 CONTROLLER_ROLE="role.yaml"
-CONTROLLER_ROLE_BINDING="role_binding.yaml"
 
 ## Output ENV
 YQ_BIN=./bin/yq
@@ -16,6 +15,3 @@ if ! [ -f $YQ_BIN ]; then
 fi
 
 curl --silent --output "$OUTPUT_DIR/$OUTPUT_PREFIX$CONTROLLER_ROLE" "$UPSTREAM/$CONTROLLER_ROLE"
-curl --silent --output "$OUTPUT_DIR/$OUTPUT_PREFIX$CONTROLLER_ROLE_BINDING" "$UPSTREAM/$CONTROLLER_ROLE_BINDING"
-
-$YQ_BIN -i '.subjects[0].name = "controller-manager"' "$OUTPUT_DIR/$OUTPUT_PREFIX$CONTROLLER_ROLE_BINDING"
