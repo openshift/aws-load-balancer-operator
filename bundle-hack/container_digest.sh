f0d664489a16b62615e259b5b66cf10dfd3d0c4a
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2025-08-26 11:54:31 +0100

    NE-2063: Add container_digest file and bundle nudging config
    Nudging allows to create dependencies between konflux components.
    The nudging relationships between components are defined in
    the Konflux cluster.
    
    This commit adds the operator on-push pipeline configuration
    which will result in the update of the operator's image digest
    in the bundle-hack/container_digest.sh file after a successful
    operator image build.
    Konflux will automatically create a PR to update the operator
    image digest to the latest.

---
+++ bundle-hack/container_digest.sh
@@ -0,0 +1,5 @@
+# Do not remove comment lines, they are there to reduce conflicts
+# Operator
+export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-2-rhel-9/aws-load-balancer-operator-container-aws-lb-optr-1-2-rhel-9@sha256:4202d0a40e357468b319269ab3e22e79302a8ec5e9ea0a300b92a79726e4e94a'
+# Controller
+export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-2-rhel-9/aws-load-balancer-controller-container-aws-lb-optr-1-2-rhel-9@sha256:1de0313bc3bcfe9f4d7a531338f5a3df63bdd3f7f1b4371e1b7c309c16ff5a90'
