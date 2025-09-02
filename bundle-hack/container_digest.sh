911d90c16d5bc979570f522bc0bef5e561468858
Grzegorz Piotrowski <97438342+grzpiotrowski@users.noreply.github.com>
2025-09-02 22:08:31 +0100

    NE-2063: OLM bundle build in Konflux (#193)
    * NE-2063: Add container_digest file and bundle nudging config
    
    Nudging allows to create dependencies between konflux components.
    The nudging relationships between components are defined in
    the Konflux cluster.
    
    This commit adds the operator on-push pipeline configuration
    which will result in the update of the operator's image digest
    in the bundle-hack/container_digest.sh file after a successful
    operator image build.
    Konflux will automatically create a PR to update the operator
    image digest to the latest.
    
    * NE-2063: Add update_bundle script for Konflux
    
    Add update_bundle.sh and the related files for OLM bundle generation
    for Konflux based on the cpaas-midstream approach.
    
    https://gitlab.cee.redhat.com/cpaas-midstream/aws-load-balancer-operator/-/blob/e553d3c6c5b36c8b1323a1c88c8f05c07cced12f/distgit/containers/aws-load-balancer-operator-bundle/render_templates

---
+++ bundle-hack/container_digest.sh
@@ -0,0 +1,7 @@
+# Do not remove comment lines, they are there to reduce conflicts
+# Operator
+export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-2-rhel-9/aws-load-balancer-operator-container-aws-lb-optr-1-2-rhel-9@sha256:4202d0a40e357468b319269ab3e22e79302a8ec5e9ea0a300b92a79726e4e94a'
+# Controller
+export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-2-rhel-9/aws-load-balancer-controller-container-aws-lb-optr-1-2-rhel-9@sha256:1de0313bc3bcfe9f4d7a531338f5a3df63bdd3f7f1b4371e1b7c309c16ff5a90'
+# kube-rbac-proxy
+export KUBE_RBAC_PROXY_IMAGE_PULLSPEC='registry.redhat.io/openshift4/ose-kube-rbac-proxy@sha256:56653df97e4498ecd67b9645d75e44f65c625594e312dbcf0f09937b57d76010'
