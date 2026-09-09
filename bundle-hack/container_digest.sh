4feae982300b25b2d4eaa8b905e58af5da09a0c9
Grzegorz Piotrowski <97438342+grzpiotrowski@users.noreply.github.com>
2026-09-09 17:13:21 +0100

    NE-2865: Configure operator build pipelines for arm64 support (#329)
    * Red Hat Konflux kflux-prd-rh03 update aws-load-balancer-operator-container-aws-lb-optr-1-3-rhel-9
    
    Signed-off-by: red-hat-konflux-kflux-prd-rh03[bot] <206760901+red-hat-konflux-kflux-prd-rh03[bot]@users.noreply.github.com>
    
    * NE-2865: Change tekton pipelines task versions to the latest
    
    * NE-2865: Re-add customizations previously applied to the pipelines
    
    This commit re-adds previously applied cusotmizations which
    were removed with the pipeline regeneration for the multi
    platform builds.
    
    The customizations added from commits:
    - NE-2084: Enforce hermetic builds and source images for Conforma compliance: https://github.com/openshift/aws-load-balancer-operator/commit/748dac4e782ce187e184ba3bf6460123bf377778
    - NE-2086: Prevent operator builds on change to bundle or catalog only: https://github.com/openshift/aws-load-balancer-operator/commit/2f6d6363aac9881b026dd1a0fe8b0ee2b275acad
    - NE-2075: Make container image labels dynamic: https://github.com/openshift/aws-load-balancer-operator/commit/959bb68be49188e37fc3ce57f9e9aa69b6352810
    - send sast-snyk-check task results to Snyk UI:  https://github.com/openshift/aws-load-balancer-operator/commit/e9601cb31698c4b83ad5939111247b1b85c15df8
    
    * NE-2865: Add arm64 platform to operator build pipelines
    
    - Add `linux/arm64` to the build-platforms in the multi-platform pipeline
    - Adapt the Containerfile to not hardcode the target architecture
    
    * NE-2865: Change Operator and Operand digests to manifest list image
    
    ---------
    
    Signed-off-by: red-hat-konflux-kflux-prd-rh03[bot] <206760901+red-hat-konflux-kflux-prd-rh03[bot]@users.noreply.github.com>
    Co-authored-by: red-hat-konflux-kflux-prd-rh03[bot] <206760901+red-hat-konflux-kflux-prd-rh03[bot]@users.noreply.github.com>

--- bundle-hack/container_digest.sh
+++ bundle-hack/container_digest.sh
@@ -1,8 +1,8 @@
 # Do not remove comment lines, they are there to reduce conflicts
 # Operator
-export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/albo/aws-load-balancer-rhel9-operator@sha256:46df6d22683c7a8a1aeaee63ff6bd2972a842cb055924f1f17b9322877fc1cec'
+export OPERATOR_IMAGE_PULLSPEC='registry.redhat.io/albo/aws-load-balancer-rhel9-operator@sha256:300192ffc1bf1b7012dd1d034b70febab17844d885e8bc833fb2903b995f2020'
 # Controller
-export OPERAND_IMAGE_PULLSPEC='registry.redhat.io/albo/aws-load-balancer-controller-rhel9@sha256:8f412218643d64518109d1fe4b97276a9c430fa4bf1192afc34ce6c9e1e57427'
+export OPERAND_IMAGE_PULLSPEC='registry.redhat.io/albo/aws-load-balancer-controller-rhel9@sha256:74b3d9e135504e47dd7f0d96d535907883bcf6c70e2ca2b3b6e3269de6204872'
 # kube-rbac-proxy
 # Latest version of v4.19 tag is used.
 # Catalog link (health grade A): https://catalog.redhat.com/en/software/containers/openshift4/ose-kube-rbac-proxy-rhel9/652809a5244cb343fb4a4b66?image=6a291e91c7ee40ca259b3f3a
